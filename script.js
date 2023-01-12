b3cf592e691ed38ec6a85b299514383295ff6bf5
Erik Sales <eriksales1@hotmail.com>
2023-01-12 16:27:26 -0500

    style finished and began JS functions

---
+++ script.js
@@ -0,0 +1,11 @@
+const operators = document.getElementById(
+    "btn-delete", "btn-divide",
+     "btn-multiply", "btn-plus", "btn-minus",
+      "btn-dot", 
+    );
+
+const number = document.getElementById("btn-number")
+
+const result = document.getElementById("btn-equal")
+
+const clear = document.getElementById("btn-clear")
