5eb398ce57797df84c9b76ade0a5c668cd7b73b6
Erik Sales <eriksales1@hotmail.com>
2023-01-15 15:59:58 -0500

    function already working and bug fixes

--- script.js
+++ script.js
@@ -1,24 +1,48 @@
-const operators = document.getElementById(
-    "btn-delete", "btn-divide",
-     "btn-multiply", "btn-plus", "btn-minus",
-      "btn-dot", 
-    );
 
 const number = document.getElementById("btn-number");
-
+const display = document.getElementById("display");
 const result = document.getElementById("btn-equal");
-
 const clear = document.getElementById("btn-clear");
-
 const previousText = document.getElementById("previous-operation");
 const currentText = document.getElementById("current-operation");
 
-function clear() {
-  previousText.innerHTML = '';
-  currentText.innerHTML = '0';
 
-}
+const buttons = Array.from(document.getElementsByClassName("button"));
 
-function del() {
+buttons.map( button => {
+  button.addEventListener("click", (event) => {
+   switch(event.target.innerText){
+    case "AC":
+          display.innerText = '';
+          break;
 
-}
+    case "DEL":
+          display.innerText = display.innerText.slice(0, -1);
+          break;
+
+    case "=":
+          display.innerText = eval(display.innerText)
+          break;
+
+
+    default:
+      display.innerText += event.target.innerText
+   }
+
+  })  
+})
+
+const operators = Array.from(document.getElementById(
+  "btn-multiply", "btn-plus", "btn-minus",
+   "btn-dot", 
+ ));
+
+
+
+display.addEventListener('keydown', (event) => {
+ display.innerText =  event.key 
+  
+  
+  //display.innerText = event.innerText
+})
+
